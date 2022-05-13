323333a42ba35ea68e52a05a14ef2efd3b4732f4
Jordan Mone <jordanmone@gmail.com>
2022-05-12 23:07:46 -0400

    Premier Commit

---
+++ src/bonjournom/BonjourNom.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bonjournom;
+import aleatoire.*;
+
+
+
+
+/**
+ *
+ * @author mtlja
+ */
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Person jordan = new Person (args[0],args[1], 23);
+        
+        System.out.println("Prenom : " + jordan.getFirstName() );
+        System.out.println("Nom : "+ jordan.getLastName() );
+        System.out.println("Age : " + jordan.getAge());
+
+
+        
+        //System.out.println("Bonjour Jordan Mone");
+        
+             
+    }
+    
+}
